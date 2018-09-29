3db93f2ad648cc7fc5bcd902076fab2a853fa2c1
William Welling <wwelling@outlook.com>
2018-09-29 11:19:16 -0500

    using UUID over String

--- domain/src/main/java/org/folio/rest/domain/model/AbstractBaseEntity.java
+++ domain/src/main/java/org/folio/rest/domain/model/AbstractBaseEntity.java
@@ -1,5 +1,7 @@
 package org.folio.rest.domain.model;
 
+import java.util.UUID;
+
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -8,19 +10,19 @@ import javax.persistence.MappedSuperclass;
 import org.hibernate.annotations.GenericGenerator;
 
 @MappedSuperclass
-public abstract class BaseEntity {
+public abstract class AbstractBaseEntity {
 
   @Id
   @GeneratedValue(generator = "UUID")
   @GenericGenerator(name = "UUID", strategy = "org.folio.rest.domain.generator.CustomUUIDGenerator")
   @Column(updatable = false, nullable = false, insertable = true)
-  private String id;
+  private UUID id;
 
-  public String getId() {
+  public UUID getId() {
     return id;
   }
 
-  public void setId(String id) {
+  public void setId(UUID id) {
     this.id = id;
   }
 
